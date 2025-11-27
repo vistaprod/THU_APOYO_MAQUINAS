e488e447358e7e0bd47d892b92e074aa36cf6fdf
vistaprod <vistaprod.io@gmail.com>
2025-11-27 19:49:07 +0100

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tareas-app-v53';
+const CACHE_NAME = 'tareas-app-v54';
 const urlsToCache = [
     './',
     './index.html',
@@ -34,6 +34,7 @@ self.addEventListener('fetch', event => {
         fetch(event.request).catch(() => caches.match(event.request))
     );
 });
+
 
 
 
